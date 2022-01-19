90879041e68221c92b5d28ac98aa360f430ebe92
lan1208883 <lan.saskatoon@gmail.com>
2022-01-18 23:00:27 -0600

    update port

--- getting_api.py
+++ getting_api.py
@@ -21,5 +21,5 @@ class Greet(Resource):
 api.add_resource(Greet, '/greet',)
 if __name__ == '__main__' :
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=6666)
+    app.run(host='0.0.0.0', port=8888)
 
